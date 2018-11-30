4b774e70c50ef6b8867bf6caad8d94cb4546b70f
jerry015 <42257913+jerry015@users.noreply.github.com>
2018-11-30 18:38:26 +0800

    Update auto_pic.py

--- auto_pic.py
+++ auto_pic.py
@@ -1,28 +1,126 @@
-import urllib2
+import urllib
+import urllib3
 import ssl
 import time
 from bs4 import BeautifulSoup
-import cookielib
+from http import cookiejar
+import os
 
-auto_atk_url = 'https://club.autohome.com.cn/bbs/thread/da7fd1e39664037e/77347159-1.html#pvareaid=102410'
 
 
-# request.add_header('User-Agent', 'Mozilla/5.0')
-# request.add_header('Host', 'club.autohome.com.cn')
-# request.add_header('Connection', 'keep-alive')
-# request.add_header('Accept-Language', 'zh-CN,zh;q=0.9,en;q=0.8')
-# request.add_header('Cookie', 'sessionid=6D7AB08D-3FC4-A8D7-FCE1-AA715A46C055%7C%7C2015-11-28+18%3A02%3A11.685%7C%7C0; tagid=9FB13E3B-CDEE-410E-5EBD-6FB92B1F5AAC; fvlid=1484443726344jIR2BkA4; __utma=1.97715377.1448704926.1471261225.1484443731.8; __ah_uuid=6DFFDAC8-38E9-41D3-8F57-1B44D5CA6EAF; sessionip=123.185.61.231; sessionvid=0BD38AAA-1090-4495-AAB9-6CA6182B5D04; area=210203; ahpau=1; sessionuid=6D7AB08D-3FC4-A8D7-FCE1-AA715A46C055%7C%7C2015-11-28+18%3A02%3A11.685%7C%7C0; papopclub=2767926F557307AFEFE3F72C04B371BD; pbcpopclub=41ab843a-e71b-40d1-af95-e79d005ab989; pepopclub=667F624E6A791CC40B6386C371BDF79A; pvidchain=3311254,2341162,102410,102410,102410,102410,102410,102410,102410,102410; ahpvno=14; ref=0%7C0%7C0%7C0%7C2018-11-21+18%3A39%3A20.736%7C2018-11-21+18%3A23%3A06.442; ahrlid=1542796755160esw6W5NFDA-1542797143425')
 
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-request = urllib2.Request(auto_atk_url)
+def download_auto_ark(auto_atk_url, folder):
+    # auto_atk_url = 'https://club.autohome.com.cn/bbs/thread/da7fd1e39664037e/77347159-1.html#pvareaid=102410'
+
+    cj = cookiejar.CookieJar()
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+    request = urllib.request.Request(auto_atk_url)
+    request.add_header('User-Agent', 'Mozilla/5.0')
+    request.add_header('Host', 'club.autohome.com.cn')
+    request.add_header('Connection', 'keep-alive')
+    request.add_header('Accept-Language', 'zh-CN,zh;q=0.9,en;q=0.8')
+    try:
+        html = opener.open(request)
+    except:
+        return
+    
+
+    soup = BeautifulSoup(html.read(),'html.parser')
+    img = soup.find_all('img')
+
+    def pic_herf(y):
+        return y.get('src9')
+
+    img_src = list(map(pic_herf,img))
+
+
+    def pic_img(x):
+        return 'userphotos' in x
+    def check_none(z):
+        return z
+    img_filter1 = list(filter(check_none,img_src))
+    img_filter2 = list(filter(pic_img, img_filter1))
+
+    def add_https(a):
+        return "https:" + a
+
+    img_filter3 = list(map(add_https,img_filter2))
+
+
+    for x in img_filter3:
+        # cj2 = cookiejar.CookieJar()
+        # opener2 = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj2))
+        print("processing " + str(img_filter3.index(x) + 1) + '/' + str(len(img_filter3)) + ' pic')
+        request = urllib.request.Request(x)
+        request.add_header('User-Agent', 'Mozilla/5.0')
+        # request.add_header('Host', 'club.autohome.com.cn')
+        request.add_header('Connection', 'keep-alive')
+        request.add_header('Accept-Language', 'zh-CN,zh;q=0.9,en;q=0.8')
+        request.add_header('Referer','https://club.autohome.com.cn/bbs/thread/49a735c3cf5dff09/77253048-1.html')
+        # opener.open(request)
+
+        # request2 = urllib.request.Request(pic_url)
+        # print(pic_url)
+        # request2.add_header('User-Agent', 'Mozilla/5.0')
+        # request2.add_header('Host', 'club.autohome.com.cn')
+        # request2.add_header('Connection', 'keep-alive')
+        # request2.add_header('Accept-Language', 'zh-CN,zh;q=0.9,en;q=0.8')
+
+        try:
+            html = urllib.request.urlopen(request,timeout=60)
+        except:
+            continue
+        # html = urllib.request.urlopen(request)
+
+        file_name = folder + str(time.time()).split('.')[0] + '.' + x.split('.')[-1]
+
+        if os.path.exists(folder) == False:
+            os.makedirs(folder)
+
+        with open(file_name, 'wb') as file:
+            file.write(html.read())
+            file.flush()
+            file.close()
+        
+        time.sleep(1)
+
+
+folder = '../photo/' + str(time.time()).split('.')[0] + '/'
+
+ch_etk_url = 'https://club.autohome.com.cn/jingxuan/104/'
+
+cj = cookiejar.CookieJar()
+opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+request = urllib.request.Request(ch_etk_url)
 request.add_header('User-Agent', 'Mozilla/5.0')
 request.add_header('Host', 'club.autohome.com.cn')
 request.add_header('Connection', 'keep-alive')
 request.add_header('Accept-Language', 'zh-CN,zh;q=0.9,en;q=0.8')
 html = opener.open(request)
 
-soup = BeautifulSoup(html.read())
-img = soup.find_all('img')
+soup = BeautifulSoup(html.read(),'html.parser')
+img = soup.find_all('a')
 
-print img
+def pic_herf(y):
+    return y.get('href')
+
+img_src = list(map(pic_herf,img))
+
+
+def pic_img(x):
+    return 'bbs' in x
+def check_none(z):
+    return z
+img_filter1 = list(filter(check_none,img_src))
+img_filter2 = list(filter(pic_img, img_filter1))
+
+def add_https(a):
+    return "https:" + a
+
+img_filter3 = list(map(add_https,img_filter2))
+img_filter3 = img_filter3[5:]
+img_filter3 = list(set(img_filter3))
+
+for x in img_filter3:
+    print("processing " + str(img_filter3.index(x) + 1) + '/' + str(len(img_filter3)) + ' url')
+    download_auto_ark(x,folder)
